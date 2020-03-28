bb0f8693a6682c9bc1f1517136465b983713bd62
Mandata singh <49266236+thakurmayank@users.noreply.github.com>
2020-03-28 18:03:51 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -4,8 +4,22 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
+
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args){
+        MyBinarySearchTree MyBinarySearchTree=new MyBinarySearchTree();
+        Scanner sc=new Scanner(System.in);
+        int input=7;
+        while(input>0){
+            MyBinarySearchTree.add(sc.nextInt());
+            input--;
+        }
+    }
 }
