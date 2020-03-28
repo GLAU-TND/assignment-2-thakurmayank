11307a943740b52638f6cbf22b76e4fc3793f0de
Mandata singh <49266236+thakurmayank@users.noreply.github.com>
2020-03-28 18:06:26 +0530

    Update MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -4,7 +4,57 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
+
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
-public class MyBinarySearchTree {
+public class MyBinarySearchTree{
+    public static StringBuilder preOrderString;
+    public TreeNode root;
+    public int c=0;
+    public MyBinarySearchTree() {
+        this.root=null;
+        preOrderString=new StringBuilder();
+    }
+    public static void pre(TreeNode root) {
+        if (root==null) {
+            return;
+        }
+        preOrderString.append(root.getValue()+"   ");
+        pre(root.getLeft());
+        pre(root.getRight());
+    }
+    public TreeNode addService(final TreeNode treeRoot, final int addValue) {
+        if (treeRoot==null)
+        {
+            return new TreeNode(addValue);
+        }
+        if (addValue<treeRoot.getValue())
+        {
+            treeRoot.setLeft(addService(treeRoot.getLeft(),addValue));
+        }
+        else if (addValue==treeRoot.getValue())
+        {
+            return treeRoot;
+        }
+        else {
+            treeRoot.setRight(addService(treeRoot.getRight(), addValue));
+        }
+
+        return treeRoot;
+    }
+    public void add(final int value) {
+        this.root = addService(this.root,value);
+    }
+    public void post(TreeNode root) {
+        if (root==null)
+        {
+            return;
+        }
+        post(root.getLeft());
+        post(root.getRight());
+        System.out.print(root.getValue()+"   ");
+    }
 }
