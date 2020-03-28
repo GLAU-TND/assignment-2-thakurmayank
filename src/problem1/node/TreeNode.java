cd70484c02c9516eedefef87b90e96dc5544c320
Mandata singh <49266236+thakurmayank@users.noreply.github.com>
2020-03-28 18:08:27 +0530

    Update TreeNode.java

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -4,7 +4,35 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
+
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<T>
+{
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int value)
+    {
+        this.value=value;
+        this.left=this.right=null;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value=value;
+    }
+    public TreeNode getLeft() {
+        return left;
+    }
+    public void setLeft(TreeNode left) {
+        this.left=left;
+    }
+    public TreeNode getRight() {
+        return right;
+    }
+    public void setRight(TreeNode right) {
+        this.right=right;
+    }
 }
